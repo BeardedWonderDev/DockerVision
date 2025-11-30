a317e4966f65df686b8de1891252f9ccfa760aef
BeardedWonder <the@beardedwonder.com>
2025-11-29 22:21:29 -0800

    vision pro xcode project

---
+++ visionPro/DockerVision/DockerVision/DockerVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockerVisionApp.swift
+//  DockerVision
+//
+//  Created by Brian Pistone on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DockerVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ visionPro/DockerVision/DockerVisionTests/DockerVisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  DockerVisionTests.swift
+//  DockerVisionTests
+//
+//  Created by Brian Pistone on 11/29/25.
+//
+
+import Testing
+@testable import DockerVision
+
+struct DockerVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
